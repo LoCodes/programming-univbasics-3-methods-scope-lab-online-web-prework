c1a4d096b02f408c9f621d929eaa026b791f4d7c
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-09 09:27:23 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,7 +4,7 @@ phrase =
 
 def mario("It's-a me, Mario!")
   toadstool("Thank You Mario! But Our Princess Is In Another Castle!")
-  any_phrase("puts out 'It's Dangerous To Go Alone! Take This.")
+  link("puts out 'It's Dangerous To Go Alone! Take This.")
 end
   
 
