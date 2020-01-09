a30e5e54a7244160d67f6c8e888193137d415b2c
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-09 09:01:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,3 +1,9 @@
-def anyphrase(phrase)
-  puts"Do A Barrel Roll!\n"
+def mario
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase
 end
+
+def toadstool
+  puts status
+end
+
