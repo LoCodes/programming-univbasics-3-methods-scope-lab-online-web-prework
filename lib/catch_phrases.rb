f5382bfd3e375b003578f95685ae66c6da2a7150
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-09 09:28:48 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,9 @@
-any_phrase = "phrase"
 
-phrase = 
 
-def mario("It's-a me, Mario!")
-  toadstool("Thank You Mario! But Our Princess Is In Another Castle!")
-  link("puts out 'It's Dangerous To Go Alone! Take This.")
-end
+#def mario("It's-a me, Mario!")
+#  toadstool("Thank You Mario! But Our Princess Is In Another Castle!")
+#  link("puts out 'It's Dangerous To Go Alone! Take This.")
+#end
   
 
 def mario
